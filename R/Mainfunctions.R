9cb4c8882d7f86a9e65d8d025dd99f7b06bf5df5
nityakodali <124936302+nityakodali@users.noreply.github.com>
2024-07-16 20:27:29 -0400

    Update Mainfunctions.R

--- R/Mainfunctions.R
+++ R/Mainfunctions.R
@@ -1,1 +1,8 @@
+#Factor Analysis of Florida Election Data
 
+
+#This first section explores the importing and setup of the data.
+#Here, I am going to set up some summary stat code
+#first 2 are pie chats as described:
+#One function will be for individual county in a certain year stats
+#another function will be total year stats
