4209c3cd8262384e1cf1152b8d0f830c8f067982
nityakodali <124936302+nityakodali@users.noreply.github.com>
2024-07-16 20:16:48 -0400

    Delete setup.R

--- setup.R
+++
@@ -1,9 +0,0 @@
-#' Setup Function
-#'
-#' @return nothing, but prints out directions on what to do.
-#' @export
-#'
-#' @examples setup()
-setup<-function(){
-  print("download fullVRreport.xlsx into your working directory and copy the filepath of fullVRreport.xlsx. insert the directory in quotes as the argument for clean_and_set_data function.")
-}
